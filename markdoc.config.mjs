2e6d70ab5f4085b9cd89315100b1712a562856af
Raphael <raphael.t.ruban@gmail.com>
2023-07-25 21:12:11 +0200

    changes syntax coloring to shiki

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,24 +1,28 @@
 import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
+import shiki from '@astrojs/markdoc/shiki';
 
 export default defineMarkdocConfig({
+  extends: [
+    shiki({
+      // Choose from Shiki's built-in themes (or add your own)
+      // Default: 'github-dark'
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'one-dark-pro',
+      // Enable word wrap to prevent horizontal scrolling
+      // Default: false
+      wrap: true,
+      // Pass custom languages
+      // Note: Shiki has countless langs built-in, including `.astro`!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+    }),
+  ],
   tags: {
     aside: {
-      // render: Aside,
       render: component('./src/components/posts/Aside.astro'),
       attributes: {
         type: { type: String },
         title: { type: String },
-      },
-    },
-  },
-  nodes: {
-    fence: {
-      // render: CodeBlock,
-      render: component('./src/components/posts/CodeBlock.astro'),
-      attributes: {
-        content: { type: String },
-        language: { type: String },
-        process: { type: Boolean },
       },
     },
   },
