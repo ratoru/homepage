1da27a60bc34848327d0b91e8bb5551a39a03444
Raphael <raphael.t.ruban@gmail.com>
2025-05-04 11:33:41 -0700

    fix: change rss link

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,22 +3,22 @@ import { siteConfig } from "@/site.config";
 import rss from "@astrojs/rss";
 
 export const GET = async () => {
-	const posts = await getAllPosts();
+  const posts = await getAllPosts();
 
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.publishDate,
-			link: `posts/${post.id}/`,
-		})),
-		customData: [
-			"<language>en-us</language>",
-			`<atom:link href="${new URL("rss.xml", siteConfig.url)}" rel="self" type="application/rss+xml" />`,
-		].join(""),
-		stylesheet: "/rss/styles.xsl",
-	});
+  return rss({
+    title: siteConfig.title,
+    description: siteConfig.description,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.publishDate,
+      link: `blog/${post.id}/`,
+    })),
+    customData: [
+      "<language>en-us</language>",
+      `<atom:link href="${new URL("rss.xml", siteConfig.url)}" rel="self" type="application/rss+xml" />`,
+    ].join(""),
+    stylesheet: "/rss/styles.xsl",
+  });
 };
