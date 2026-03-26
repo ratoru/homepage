f99be7d52a2cc38da390eaf5799001c8fb085ead
Raphael <raphael.t.ruban@gmail.com>
2026-03-25 18:19:45 -0700

    fix(core): remove csp again

--- astro.config.ts
+++ astro.config.ts
@@ -26,99 +26,98 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.url,
-	security: { csp: true },
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		icon(),
-		sitemap(),
-		mdx(),
-		robotsTxt(),
-		webmanifest({
-			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
-			name: siteConfig.title,
-			short_name: "Astro_Cactus", // optional
-			description: siteConfig.description,
-			lang: siteConfig.lang,
-			icon: "public/icon.svg", // the source for generating favicon & icons
-			icons: [
-				{
-					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
-					sizes: "180x180",
-					type: "image/png",
-				},
-				{
-					src: "icons/icon-192.png",
-					sizes: "192x192",
-					type: "image/png",
-				},
-				{
-					src: "icons/icon-512.png",
-					sizes: "512x512",
-					type: "image/png",
-				},
-			],
-			start_url: "/",
-			background_color: "#1d1f21",
-			theme_color: "#2bbc8a",
-			display: "standalone",
-			config: {
-				insertFaviconLinks: false,
-				insertThemeColorMeta: false,
-				insertManifestLink: false,
-			},
-		}),
-	],
-	markdown: {
-		rehypePlugins: [
-			rehypeHeadingIds,
-			[rehypeAutolinkHeadings, { behavior: "wrap", properties: { className: ["not-prose"] } }],
-			[
-				rehypeExternalLinks,
-				{
-					rel: ["noreferrer", "noopener"],
-					target: "_blank",
-				},
-			],
-			rehypeUnwrapImages,
-			rehypeMathml,
-		],
-		remarkPlugins: [
-			remarkReadingTime,
-			remarkDirective,
-			remarkDirectiveFigure,
-			remarkGithubCard,
-			remarkAdmonitions,
-			remarkQuoteAttribution,
-			remarkSidenotes,
-			remarkMath,
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: [""],
-			},
-		},
-	},
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-		plugins: [tailwind(), rawFonts([".ttf", ".woff"])],
-	},
+  site: siteConfig.url,
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    icon(),
+    sitemap(),
+    mdx(),
+    robotsTxt(),
+    webmanifest({
+      // See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
+      name: siteConfig.title,
+      short_name: "Astro_Cactus", // optional
+      description: siteConfig.description,
+      lang: siteConfig.lang,
+      icon: "public/icon.svg", // the source for generating favicon & icons
+      icons: [
+        {
+          src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "icons/icon-512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+      start_url: "/",
+      background_color: "#1d1f21",
+      theme_color: "#2bbc8a",
+      display: "standalone",
+      config: {
+        insertFaviconLinks: false,
+        insertThemeColorMeta: false,
+        insertManifestLink: false,
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, { behavior: "wrap", properties: { className: ["not-prose"] } }],
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["noreferrer", "noopener"],
+          target: "_blank",
+        },
+      ],
+      rehypeUnwrapImages,
+      rehypeMathml,
+    ],
+    remarkPlugins: [
+      remarkReadingTime,
+      remarkDirective,
+      remarkDirectiveFigure,
+      remarkGithubCard,
+      remarkAdmonitions,
+      remarkQuoteAttribution,
+      remarkSidenotes,
+      remarkMath,
+    ],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""],
+      },
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+    plugins: [tailwind(), rawFonts([".ttf", ".woff"])],
+  },
 });
 
 function rawFonts(ext: string[]) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// @ts-expect-error:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // @ts-expect-error:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
 }
